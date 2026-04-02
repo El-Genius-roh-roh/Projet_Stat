28abd8bf305a27e9aa642fcc193a2d61e149f33e
El-Genius-roh-roh <julesfrulli@gmail.com>
2026-04-03 00:32:25 +0200

    Ajout du clustering sur le dataframe dataACP avec la méthode k-means

--- Partie 2/ACP et graphique d'intérprétation des axes.R
+++ Partie 2/ACP et graphique d'intérprétation des axes.R
@@ -36,7 +36,7 @@ contribution = contribution %>%
 
 #Affichage des dimensions des variables qui ont le contribution maximal sur l'axe 1
 contribution %>% 
-  filter(Dim_principale == 5)
+  filter(Dim_principale == 4)
 
 
 # --------Etude des variables sur les graphs de l'ACP---------
@@ -78,3 +78,42 @@ var_axe5 = contribution %>%
   filter(Dim_principale == 5) %>% 
   pull(variable)
 fviz_pca_var(ACP, axes = c(4, 5), select.var = list(name = var_axe5), repel = T)
+
+
+
+# -------Nouvelle base de donnée avec comme variables les axes de l'ACP-------
+
+
+dataACP = as.data.frame(ACP$ind$coord) %>% 
+  rename(Créativité = Dim.1,
+         Pratique_activités = Dim.2,
+         Emotionnel_froid = Dim.3,
+         Activité_créative_et_Emotionel_social = Dim.4,
+         Encouragement_de_environement_à_la_créativité_Leardership = Dim.5)
+
+
+# Méthode du coude pour voir combien de cluster on prend. Je dois faire un échantillon sinon c'est trop gros
+dataACP_echantillon <- dataACP %>% slice_sample(n = 10000)
+fviz_nbclust(dataACP, kmeans, method = "wss") +
+  labs(title = "Méthode du coude")
+
+#Méthode du coude
+inertie <- sapply(1:10, function(k) {
+  kmeans(dataACP, centers = k, nstart = 10)$tot.withinss
+})
+
+plot(1:10, inertie, type = "b", 
+     xlab = "Nombre de clusters", 
+     ylab = "Inertie intra-classe",
+     main = "Méthode du coude")
+
+
+#Algo k-means (j'ai estimer que 4 clusters c'était bien)
+km <- kmeans(dataACP, centers = 4, nstart = 25, iter.max = 100)
+
+#Ajout du numéro du cluster correspondant pour chaque variable
+dataACP = dataACP %>% 
+  mutate(cluster = as.factor(km$cluster))
+
+
+
