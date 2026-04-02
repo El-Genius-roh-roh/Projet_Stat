5426de8cefc18a00a423723ad082d0770fb93e17
El-Genius-roh-roh <julesfrulli@gmail.com>
2026-04-02 22:13:55 +0200

    ACP sur les variables et trie des variables dans les dimensions dans lesquelles elles ont le plus contribué

--- Partie 2/ACP et graphique d'intérprétation des axes.R
+++ Partie 2/ACP et graphique d'intérprétation des axes.R
@@ -20,9 +20,56 @@ fviz_contrib(ACP, choice = "var", axes = 5)
 
 #Création d'un dataframe qui contient les contributions de chaque variable
 #à chaque dimension
-contribution = as.data.frame(ACP$var$contrib)
+contribution = as.data.frame(ACP$var$contrib) %>% 
+  rownames_to_column("variable") 
+# Cette ligne permet juste de nommer la collone qui donne
+# le nom aux lignes (c'est à dire la collone des variables)
 
 #Ajout d'une collone qui correspond à la dimension à laquelle la varible à le
-#plus contribué
+#plus contribué + ajout d'une collone qui indique la valeur de la plus grande 
+#contribution
+contribution = contribution %>% 
+  mutate(Dim_principale = apply(select(., Dim.1, Dim.2, Dim.3, Dim.4, Dim.5), 1, which.max)) %>% 
+  mutate(Plus_grande_contrib = apply(select(., Dim.1, Dim.2, Dim.3, Dim.4, Dim.5), 1, max)) %>% 
+  filter(Plus_grande_contrib > 0.6) #Seuil
+
+
+#Affichage des dimensions des variables qui ont le contribution maximal sur l'axe 1
 contribution %>% 
-  mutate(dim_principale = apply(select(., starts_with("Dim")), 1, which.max))
+  filter(Dim_principale == 1)
+
+
+# --------Etude des variables sur les graphs de l'ACP---------
+
+
+#Création d'un vecteur qui contient le nom de toutes les variables qui ont pour
+#contribution max l'axe 1
+
+var_axe1 = contribution %>% 
+  filter(Dim_principale == 1) %>% 
+  pull(variable) #Cette ligne permet de renvoyer un vecteur qui contient
+# seuelement ce qui se trouve sur la collonne "variable" (dans notre cas c'est 
+# le nom de variable)
+
+
+fviz_pca_var(ACP, axes = c(1, 2), select.var = list(name = var_axe1), repel = T)
+#repel = T évite juste que les variables se chevauchent
+
+
+#On fait la même pour les autres axes
+
+var_axe2 = contribution %>% 
+  filter(Dim_principale == 2) %>% 
+  pull(variable)
+fviz_pca_var(ACP, axes = c(1, 2), select.var = list(name = var_axe2), repel = T)
+
+
+var_axe3 = contribution %>% 
+  filter(Dim_principale == 3) %>% 
+  pull(variable)
+fviz_pca_var(ACP, axes = c(3, 4), select.var = list(name = var_axe3), repel = T)
+
+var_axe4 = contribution %>% 
+  filter(Dim_principale == 4) %>% 
+  pull(variable)
+fviz_pca_var(ACP, axes = c(3, 4), select.var = list(name = var_axe4), repel = T)
